110fa2d2716b4ed359334061e5d4635cd9773453
Tomas Otano <otano@mx1.ibm.com>
2019-11-12 19:53:55 +0000

    Forgot to modify Scott by 'Tomas'

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -31,7 +31,7 @@ describe('controllers', function() {
         request(server)
           .get('/hello')
           .query({
-            name: 'Scott'
+            name: 'Tomas'
           })
           .set('Accept', 'application/json')
           .expect('Content-Type', /json/)
