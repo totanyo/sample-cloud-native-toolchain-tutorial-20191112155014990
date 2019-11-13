02f9e31b5413973102bc1517c5f15e16db09f2e7
Tomas Otano <otano@mx1.ibm.com>
2019-11-13 15:37:42 +0000

    Line 42 in test: res.body.should.eql('Hello there, whats up doc, Tomas!');

--- test/api/controllers/hello_world.js
+++ test/api/controllers/hello_world.js
@@ -39,7 +39,7 @@ describe('controllers', function() {
           .end(function(err, res) {
             should.not.exist(err);
 
-            res.body.should.eql('Hello there, Tomas!');
+            res.body.should.eql('Hello there, whats up doc, Tomas!');
 
             done();
           });
